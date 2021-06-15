68d4bc254e3fbc61eda59e13320a4c2fc0b230c2
Piotr <piotr_as@yahoo.com>
2021-06-15 15:57:35 +0200

    added jquery

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
 import '../scss/app.scss'
 /* Demo JS */
 import './navbar.js'
+import $ from "jquery";
 
 /* Your JS Code goes here */
 console.log('%c Hello from app.js', 'color: #ffc600; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
 
+$('.navbar__link').on('click', function() {
+    $('.navbar__link').removeClass('.navabr__link--active');
+    $(this).addClass('.navbar__link--active');
+});
--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,16 +1,17 @@
 console.log('%c Hello from Demo.js', 'color: #bada55; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
 
 
-const activeclass = document.querySelectorAll('.navbar__link--active');
+// const activeclass = document.querySelectorAll('.navbar__link--active');
    
 
-function activateClass(e) {
-    var previous = e.target.previousElementSibling;
-    var next = e.target.nextElementSibling;
-    e.target.classList.add('navbar__link--active');
-    previous.classList.remove('navbar__link--active');
-    next.classList.remove('navbar__link--active');
-}
+// function activateClass(e) {
+//     var previous = e.target.previousElementSibling;
+//     var next = e.target.nextElementSibling;
+//     e.target.classList.add('navbar__link--active');
+//     previous.classList.remove('navbar__link--active');
+//     next.classList.remove('navbar__link--active');
+// }
 
 
-activeclass.forEach(link => link.addEventListener('click', activateClass)
+// activeclass.forEach(link => link.addEventListener('click', activateClass)
+
