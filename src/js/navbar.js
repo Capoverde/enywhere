56a7aed2550c15e6690973d368af0821e536ad50
Piotr <piotr_as@yahoo.com>
2021-06-16 16:00:56 +0200

    last changes today done in office

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,9 +1,9 @@
 console.log('%c Hello from Demo.js', 'color: #bada55; background: #000; padding: 5px 10px; margin: 5px 25px; font-size:2em;')
 
 const links = document.getElementsByClassName("navbar__link");
-for (var i = 0; i < links.length; i++) {
+for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("navbar__link--active");
+  const current = document.getElementsByClassName("navbar__link--active");
   current[0].className = current[0].className.replace(" navbar__link--active", "");
   this.className += " navbar__link--active";
   });
